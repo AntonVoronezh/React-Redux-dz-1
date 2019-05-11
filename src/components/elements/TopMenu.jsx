fde7f52326046d898d80623e3a80c76e897df3c8
AntonVoronezh <pstut@mail.ru>
2019-05-11 15:47:59 +0300

    refactoring TopMenu

--- src/components/elements/TopMenu.jsx
+++ src/components/elements/TopMenu.jsx
@@ -6,18 +6,24 @@ import { NavLink } from 'react-router-dom';
 const TopMenu = () => {
 	return (
 		<Menu>
-			<NavLink>
+			{/* <NavLink to="/">
 				<Menu.Item content="На главную" />
-			</NavLink>
-			<NavLink>
+			</NavLink> */}
+			{/* <NavLink to="/news">
 				<Menu.Item content="Новости" />
-			</NavLink>
-			<NavLink>
-				<Menu.Item content="Профайл" />
-			</NavLink>
-			<NavLink>
-				<Menu.Item content="Логин" />
-			</NavLink>
+			</NavLink> */}
+			{/* <NavLink to="/profile" /> */}
+			{/* <Menu.Item position="right">
+				<NavLink to="/login">Логин</NavLink>
+			</Menu.Item> */}
+			{/* <NavLink to="/login" >
+				<Menu.Item content="Логин" position="right"/>
+			</NavLink> */}
+
+			<Menu.Item content="На главную" as={NavLink} to="/" activeClassName="active" />
+			<Menu.Item content="Новости" as={NavLink} to="/news" activeClassName="active" />
+			<Menu.Item content="Профиль" as={NavLink} to="/profile" activeClassName="active" />
+			<Menu.Item content="Логин" as={NavLink} to="/login" activeClassName="active" />
 		</Menu>
 	);
 };
