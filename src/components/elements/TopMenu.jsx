465e386e4b0be0edde97934f1bd7915b8cc55aa6
AntonVoronezh <pstut@mail.ru>
2019-05-11 15:25:12 +0300

    add NavLink

--- src/components/elements/TopMenu.jsx
+++ src/components/elements/TopMenu.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Menu } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 const TopMenu = () => {
 	return (
 		<Menu>
-			<Menu.Item content="На главную" />
-			<Menu.Item content="Новости" />
-			<Menu.Item content="Профайл" />
-			<Menu.Item content="Логин" />
+			<NavLink>
+				<Menu.Item content="На главную" />
+			</NavLink>
+			<NavLink>
+				<Menu.Item content="Новости" />
+			</NavLink>
+			<NavLink>
+				<Menu.Item content="Профайл" />
+			</NavLink>
+			<NavLink>
+				<Menu.Item content="Логин" />
+			</NavLink>
 		</Menu>
 	);
 };
