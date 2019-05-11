bd5eda88aacdc591bc4fcb8214126d33e5fb7146
AntonVoronezh <pstut@mail.ru>
2019-05-11 14:53:50 +0300

    add fish reducder, action

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
 import loginReducer from './loginReducer';
-import reducerUi from './reducerUi';
-import reducerForm from './reducerForm';
 
-export default combineReducers({ login: loginReducer, ui: reducerUi, form: reducerForm });
+export default combineReducers({ fish:fishReducer});
