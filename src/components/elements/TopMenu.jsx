76addf5cd13daa2c1214894e124fea021fc4b77e
AntonVoronezh <pstut@mail.ru>
2019-05-11 15:15:46 +0300

    add TopMenu

---
+++ src/components/elements/TopMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TopMenu = () => {
+	return <div>TopMenu</div>;
+};
+
+export default TopMenu;
+
+TopMenu.propTypes = {
+    name: PropTypes.string
+  };
