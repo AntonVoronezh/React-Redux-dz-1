499ae1dfedcf59f8d1a2006fd61f888b5249f1d9
AntonVoronezh <pstut@mail.ru>
2019-05-11 15:21:24 +0300

    add menu items in TopMenu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import TopMenu from './components/elements/TopMenu.jsx';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       jjjjj
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<TopMenu />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/elements/TopMenu.jsx
+++ src/components/elements/TopMenu.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Menu } from 'semantic-ui-react';
 
 const TopMenu = () => {
-	return <div>TopMenu</div>;
+	return (
+		<Menu>
+			<Menu.Item content="На главную" />
+			<Menu.Item content="Новости" />
+			<Menu.Item content="Профайл" />
+			<Menu.Item content="Логин" />
+		</Menu>
+	);
 };
 
 export default TopMenu;
 
 TopMenu.propTypes = {
-    name: PropTypes.string
-  };
+	name: PropTypes.string,
+};
