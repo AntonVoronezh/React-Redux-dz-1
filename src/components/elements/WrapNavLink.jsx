5977f44f505741cb4b081fd8fb5433289a41bcee
AntonVoronezh <pstut@mail.ru>
2019-05-11 15:58:24 +0300

    refactoring WrapNavLink

--- src/components/elements/WrapNavLink.jsx
+++ src/components/elements/WrapNavLink.jsx
@@ -1,21 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-const TopMenu = () => {
-	return (
-		<Menu>
-			<Menu.Item content="На главную" as={NavLink} exact to="/" activeClassName="active" />
-			<Menu.Item content="Новости" as={NavLink} to="/news" activeClassName="active" />
-			<Menu.Item content="Профиль" as={NavLink} to="/profile" activeClassName="active" />
-			<Menu.Item content="Логин" as={NavLink} to="/login" activeClassName="active" position="right" />
-		</Menu>
-	);
-};
+const WrapNavLink = () => <WrapNavLink as={NavLink} activeClassName="active" />;
 
-export default TopMenu;
-
-TopMenu.propTypes = {
-	name: PropTypes.string,
-};
+export default WrapNavLink;
