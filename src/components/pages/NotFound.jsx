b5e3ed4f8772a92efc6032bbe978404ce86095fd
AntonVoronezh <pstut@mail.ru>
2019-05-12 14:59:29 +0300

    refactoring NotFound

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Notfound = () => {
 	return <div>Notfound</div>;
 };
 
 export default Notfound;
-
-Notfound.propTypes = {
-    name: PropTypes.string
-  };
