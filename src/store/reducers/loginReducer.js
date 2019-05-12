7b0dfaa81730157c44a5e474188f9e9ee8c3f1e9
AntonVoronezh <pstut@mail.ru>
2019-05-12 14:51:45 +0300

    add loginReducer

---
+++ src/store/reducers/loginReducer.js
@@ -0,0 +1,17 @@
+import { login } from '../../helpers/login/login';
+import { setInStorage, getInStorage } from '../../helpers/localStorage/localStorage';
+
+import { TRY_LOGIN, LOGOUT } from '../actions/loginActions';
+
+
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+
+
+
+
+		default:
+			return state;
+	}
+};
