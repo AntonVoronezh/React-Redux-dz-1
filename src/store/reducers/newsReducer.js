042954ba555bbe506e28b82162ac0570a431bdf9
AntonVoronezh <pstut@mail.ru>
2019-05-13 15:19:44 +0300

    add initialState in newsReducer

--- src/store/reducers/newsReducer.js
+++ src/store/reducers/newsReducer.js
@@ -1,6 +1,10 @@
 import { NEWS_REQUEST, NEWS_SUCCESS, NEWS_FAILURE } from '../actions/newsActions';
 
-
+const initialState = {
+	isLoading: false,
+	news: [],
+	error: null,
+};
 
 export default (state = initialState, action) => {
 	switch (action.type) {
