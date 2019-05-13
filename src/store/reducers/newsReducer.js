daa46a86bd37e26010e7b1353fdd036a5ed1caf9
AntonVoronezh <pstut@mail.ru>
2019-05-13 15:19:20 +0300

    add newsReducer

---
+++ src/store/reducers/newsReducer.js
@@ -0,0 +1,11 @@
+import { NEWS_REQUEST, NEWS_SUCCESS, NEWS_FAILURE } from '../actions/newsActions';
+
+
+
+export default (state = initialState, action) => {
+	switch (action.type) {
+
+		default:
+			return state;
+	}
+};
