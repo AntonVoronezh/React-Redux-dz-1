16eeae5309b66762947c093d1a03f333fac367ee
AntonVoronezh <pstut@mail.ru>
2019-05-13 15:16:03 +0300

    refactoring store

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
-export default () => createStore(rootReducer, applyMiddleware(logger));
+export default () => createStore(rootReducer, applyMiddleware(thunk, logger));
