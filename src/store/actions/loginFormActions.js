8e5955f4ebff9deb863f9b66c2974f45f65ee566
AntonVoronezh <pstut@mail.ru>
2019-05-14 08:22:20 +0300

    add actions creators

--- src/store/actions/loginFormActions.js
+++ src/store/actions/loginFormActions.js
@@ -3,3 +3,7 @@ export const PASSWORD_TEXT = 'PASSWORD_TEXT';
 export const USERNAME_BACKSPACE = 'USERNAME_BACKSPACE';
 export const PASSWORD_BACKSPACE = 'PASSWORD_BACKSPACE';
 
+export const usernameTextAC = text => ({ type: USERNAME_TEXT, payload: text });
+export const passwordTextAC = text => ({ type: PASSWORD_TEXT, payload: text });
+export const usernameBackspaceAC = () => ({ type: USERNAME_BACKSPACE });
+export const passwordBackspaceAC = () => ({ type: PASSWORD_BACKSPACE });
